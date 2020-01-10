f6b13699d8ce45510703f0a7221c768c89bd0dea
zacharyarney <zachary.arney@gmail.com>
2020-01-09 21:28:03 -0800

    Node and List constructors in singlyLinkedList

---
+++ singlyLinkedList.js
@@ -0,0 +1,9 @@
+function Node(value, next = null) {
+  this.value = value;
+  this.next = next;
+}
+
+function SinglyLinkedList(head = null, tail = null) {
+  this.head = head;
+  this.tail = tail;
+}
