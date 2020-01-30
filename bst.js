d0562b6c36396fbed08a37e76cb017076f523b32
zacharyarney <zachary.arney@gmail.com>
2020-01-29 22:02:33 -0800

    adds instert method to bts

--- bst.js
+++ bst.js
@@ -4,4 +4,24 @@ class BinarySearchTree {
     this.left = left;
     this.right = right;
   }
+
+  insert(value) {
+    newNode = new BinarySearchTree(value);
+
+    if (self.value > value) {
+      if (self.left) {
+        self.left.insert(value);
+      } else {
+        self.left = newNode;
+      }
+    }
+
+    if (self.value < value) {
+      if (self.right) {
+        self.right.insert(value);
+      } else {
+        self.right = newNode;
+      }
+    }
+  }
 }
