df1ec2595405c719a08d197e6b8bc57d4689a168
zacharyarney <zachary.arney@gmail.com>
2020-02-03 13:05:04 -0800

    reached a block using bst class without node class This solution is more pythonic. JS can't really do this without a node. this commit is just to document where I stopped with this approach

--- bst/bst.js
+++ bst/bst.js
@@ -14,9 +14,7 @@ class BinarySearchTree {
       } else {
         this.left = newNode;
       }
-    }
-
-    if (this.value < value) {
+    } else if (this.value < value) {
       if (this.right) {
         this.right.insert(value);
       } else {
@@ -25,18 +23,86 @@ class BinarySearchTree {
     }
   }
 
-  contains(target) {
+  find(target) {
     if (this.value === target) {
-      return true;
+      return this;
     }
-    
+
     if (this.value > target && this.left) {
       // don't forget to return recursive call when function returns
-      return this.left.contains(target);
+      return this.left.find(target);
     }
 
     if (this.value < target && this.right) {
-        return this.right.contains(target);
+      return this.right.find(target);
+    }
+    return null;
+  }
+
+  // find parent of value
+  // if value has no children, remove connection
+  // if value has one child, set parent.left/right to child of target
+  // if value has two children
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+    // if (this.left.value === value) {
+    //   const parent = this;
+    //   const target = this.left;
+    //   if (!target.left && !target.right) {
+    //     this.left = null;
+    //   } else if (target.left && !target.right) {
+    //     this.left = target.left;
+    //   } else if (target.right && !target.left) {
+    //     this.left = target.right;
+    //   } else {
+    //     let successor = target.right.getMin();
+    //     parent.left = successor;
+    //     successor.left = target.left;
+    //     successor.right = target.right;
+    //     successor.right.getMin().left = null;
+    //   }
+    // } else if (this.right.value === value) {
+    //   const parent = this;
+    //   const target = this.right;
+    //   if (!target.left && !target.right) {
+    //     this.left = null;
+    //   } else if (target.left && !target.right) {
+    //     this.left = target.left;
+    //   } else if (target.right && !target.left) {
+    //     this.left = target.right;
+    //   } else {
+    //     let successor = target.right.getMin();
+    //     parent.right = successor;
+    //     successor.left = target.left;
+    //     successor.right = target.right;
+    //     successor.right.getMin().left = null;
+    //   }
+    // }
+    // if (this.value > value) {
+    //   this.left.removeNode(value);
+    // } else if (this.value < value) {
+    //   this.right.removeNode(value);
+    // }
+  }
+
+  removeNode(value) {
+    let cur = this;
+    console.log('cur.value = ', cur.value);
+    if (cur.value > value) {
+      cur.left.remove(value);
+    } else if (cur.value < value) {
+      cur.right.remove(value);
+    } else {
+      if (cur.left === null && cur.right === null) {
+        cur = null;
+      }
+    }
+  }
+
+  contains(target) {
+    if (this.find(target) !== null) {
+      return true;
     }
     return false;
   }
@@ -46,7 +112,15 @@ class BinarySearchTree {
     while (cur.right) {
       cur = cur.right;
     }
-    return cur.value;
+    return cur;
+  }
+
+  getMin() {
+    let cur = this;
+    while (cur.left) {
+      cur = cur.left;
+    }
+    return cur;
   }
 }
 
--- bst/test.bst.js
+++ bst/test.bst.js
@@ -7,6 +7,11 @@ bst.insert(2);
 bst.insert(3);
 bst.insert(7);
 bst.insert(6);
+bst.insert(9);
+bst.insert(8);
+bst.insert(21);
+bst.insert(29);
+bst.insert(11);
 
 assert.strictEqual(bst.left.right.value, 3);
 assert.strictEqual(bst.right.left.value, 6);
@@ -14,6 +19,12 @@ assert.strictEqual(bst.right.left.value, 6);
 assert(bst.contains(6));
 assert(bst.contains(2));
 
-assert.strictEqual(bst.getMax(), 7);
+assert.strictEqual(bst.getMax().value, 29);
+
+bst.removeNode(29);
+console.log('21.right.value', bst.find(21).right.value);
+assert.strictEqual(bst.getMax().value, 29);
+// assert.strictEqual(bst.find(8).right.value, 9);
+// assert.strictEqual(bst.find(8).left.value, 6);
 
 console.log('ALL TESTS PASS');
